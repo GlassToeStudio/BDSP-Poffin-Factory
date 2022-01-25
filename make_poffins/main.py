52dd69291997e474623ab0222550507d2b190489
GlassToeStudio <GlassToeStudio@gmail.com>
2022-01-24 21:19:51 -0600

    Change filter

--- make_poffins/main.py
+++ make_poffins/main.py
@@ -4,13 +4,13 @@ from make_poffins.berry.berry_factory import BerryFactory
 from make_poffins.berry.berry_sort_and_filter_system import \
     BerrySortAndFilterSystem
 from make_poffins.berry.interface_berry_filter import \
-    FilterBerriessBy_Rarity_GreaterThan
+    FilterBerriessBy_Rarity_LessThan
 from make_poffins.berry.interface_berry_sort import SortOnBerry_Attrs
 from make_poffins.poffin.interface_poffin_filter import (
     FilterPoffinsBy_AnyFlavorValueLessThan, FilterPoffinsBy_Level_LessThan,
     FilterPoffinsBy_MaxNSimilar, FilterPoffinsBy_NumberOfFlavors_LessThan)
-from make_poffins.poffin.interface_poffin_sort import \
-    SortOnPoffins_LevelToSmoothnessRatioSum
+from make_poffins.poffin.interface_poffin_sort import (
+    SortOnPoffins_Level, SortOnPoffins_LevelToSmoothnessRatioSum)
 from make_poffins.poffin.poffin_cooker import PoffinCooker
 from make_poffins.poffin.poffin_factory import PoffinFactory
 from make_poffins.poffin.poffin_sort_and_filter_system import \
@@ -24,21 +24,27 @@ from stats.contest_stats_sort_and_filter_system import \
 
 
 def main():
-    no_berries_rarer_than = 15
 
-    min_level = 50
+    # Berries
+    no_berries_rarer_than = 7
+
+    # Poffins
+    min_level = 100
     min_flavors = 2
     max_similar = 4
-    min_value = 20
-
-    min_rank = 1
-    max_eaten = 20
+    min_value = 30
+    # Cooking
     cook_time = 40
-    top_x = 20
+
+    # Stats
+    max_eaten = 8
+    min_rank = 1
+    # Internal rank mechanism
+    stop_at_first_n_results = 10
 
     # Berries
     berry_filters_sorters = [
-        FilterBerriessBy_Rarity_GreaterThan(no_berries_rarer_than),
+        FilterBerriessBy_Rarity_LessThan(no_berries_rarer_than),
         SortOnBerry_Attrs((('main_flavor', False), ('smoothness', False),  ('_weakened_main_flavor_value', False))),
     ]
     berry_filtering_sorting_system = BerrySortAndFilterSystem(berry_filters_sorters)
@@ -51,11 +57,12 @@ def main():
         FilterPoffinsBy_NumberOfFlavors_LessThan(min_flavors),
         SortOnPoffins_LevelToSmoothnessRatioSum(),
         FilterPoffinsBy_MaxNSimilar(max_similar),
-        FilterPoffinsBy_AnyFlavorValueLessThan(min_value)
+        FilterPoffinsBy_AnyFlavorValueLessThan(min_value),
+        SortOnPoffins_Level()
     ]
     poffin_filtering_sorting_system = PoffinSortAndFilterSystem(poffin_filters_sorters)
     poffin_factory = PoffinFactory(PoffinCooker(cook_time), berry_combinations, poffin_filtering_sorting_system)
-    poffin_permutations = poffin_factory.get_poffin_permutations_4()
+    poffin_permutations = poffin_factory.get_poffin_permutations_3()
 
     # Stats
     contest_stats_filters_sorters = [
@@ -63,7 +70,7 @@ def main():
         FilterContestStatsBy_Rank_GreaterThan(min_rank)
     ]
     contest_stats_filtering_sorting_system = ContestStatsSortAndFilterSystem(contest_stats_filters_sorters)
-    contest_stat_factory = ContestStatsFactory(poffin_permutations, contest_stats_filtering_sorting_system, top_x, min_rank, max_eaten)
+    contest_stat_factory = ContestStatsFactory(poffin_permutations, contest_stats_filtering_sorting_system, stop_at_first_n_results, min_rank, max_eaten)
     final_stats = contest_stat_factory.get_filtered_and_sorted_contest_stats()
 
     timestamp = time.strftime("%d-%b-%Y %I-%M %p", time.localtime())
