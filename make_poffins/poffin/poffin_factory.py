69a30c455293bbfdfc242ad46d964a4d2e860c30
GlassToeStudio <GlassToeStudio@gmail.com>
2022-01-28 09:16:38 -0600

    Add a multiproccesed method, but not using it.

--- make_poffins/poffin/poffin_factory.py
+++ make_poffins/poffin/poffin_factory.py
@@ -1,8 +1,11 @@
 import math
+import multiprocessing as mp
+import queue
 from itertools import combinations, permutations
 
 from make_poffins.berry.berry import Berry
-from make_poffins.constants import TOTAL_POFFINS, calculate_time
+from make_poffins.constants import (TOTAL_BERRIES, TOTAL_POFFINS,
+                                    calculate_time, chunks)
 from make_poffins.poffin.poffin import Poffin
 from make_poffins.poffin.poffin_cooker import PoffinCooker
 from make_poffins.poffin.poffin_sort_and_filter_system import \
@@ -14,9 +17,9 @@ class PoffinFactory():
         self._cooker = cooker
         self._berry_combinations = berry_combinations
         self._poffin_filter_system = poffin_filter_system
-
-        self._poffins = None
-        self._filtered_poffin_list = None
+        self._poffins = []
+        self._filtered_poffin_list = []
+        self.num_berries = int(TOTAL_BERRIES[0])
         print("\nSetting Up PoffinFactory")
 
     @property
@@ -29,8 +32,9 @@ class PoffinFactory():
         Returns:
             list[Poffin]: Unsorted or Filtered Poffin List
         """
-        if self._poffins is None:
-            self._poffins = self._generate_poffin_list()
+        if not self._poffins:
+            self._poffins = self._generate_poffin_list_serial()
+            # self._poffins = self._generate_poffin_list_parallel() TODO: Not using this, its slower.. for some reason and I think there is a bug.
         print(f"Returning {len(self._poffins)} Cooked Poffins")
         return self._poffins
 
@@ -41,15 +45,14 @@ class PoffinFactory():
         Returns:
             list[Poffin]: Filtered and Sorted Poffin List
         """
-        if self._filtered_poffin_list is None:
+        if not self._filtered_poffin_list:
             self._filtered_poffin_list = self._poffin_filter_system.get_filtered_and_sorted_poffins(self.poffins)
         print(f"Returning {len(self._filtered_poffin_list)} Filtered Poffins")
         return self._filtered_poffin_list
 
     @calculate_time
-    def _generate_poffin_list(self) -> list[Poffin]:  # noqa ES501
-        print("Cooking Poffins")
-        self._poffins = []
+    def _generate_poffin_list_serial(self) -> list[Poffin]:  # noqa ES501
+        print("Cooking Poffins", self.num_berries, mp.cpu_count())
         for recipe in self._berry_combinations:
             p = self._cooker.cook(recipe)
             if p.name == "foul poffin":
@@ -58,13 +61,39 @@ class PoffinFactory():
         return self._poffins
 
     @calculate_time
+    def _generate_poffin_list_parallel(self) -> list[Poffin]:  # noqa ES501
+        processed_list = mp.Manager().list()
+        processes = []
+        chunk_size = int(self.num_berries//(mp.cpu_count()))
+        for i, berry_chunk in enumerate(chunks(self._berry_combinations, chunk_size)):
+            if i % chunk_size == 0:
+                p = mp.Process(target=self._parallel_task, args=(berry_chunk, processed_list))
+                processes.append(p)
+                p.start()
+
+        for p in processes:
+            p.join()
+
+        self._poffins = processed_list
+        return self._poffins
+
+    def _parallel_task(self, berry_chunks, shared_list):
+        for recipe in berry_chunks:
+            p = self._cooker.cook(recipe)
+            if p.name == "foul poffin":
+                continue
+            shared_list.append(p)
+
+    @calculate_time
     def _get_poffin_permutations_n(self, n: int, poffins: list[Poffin] = None) -> tuple[Poffin, ...]:  # noqa ES501
-        global TOTAL_POFFINS
         """Every combination of n poffins"""
-        print(f"Calling {self._get_poffin_permutations_n.__name__}, there are {'0' if poffins is None  else len(poffins)} poffins")
+
+        global TOTAL_POFFINS
+
         if poffins is None:
             poffins = self.filtered_poffins
-        print(f"Permutating {len(poffins)} Filtered Poffins")
+        print(f"Permutating {len(poffins)} Filtered Berries")
+
         c = math.factorial(len(poffins)) / (math.factorial(len(poffins)-n))
         TOTAL_POFFINS[0] = c
         print(f"There are {c} permutations! Wow")
@@ -72,27 +101,22 @@ class PoffinFactory():
 
     def get_poffin_permutations_2(self, poffins: list[Poffin] = None) -> tuple[Poffin, Poffin]:
         """Every combination of 2 poffins"""
-        print(f"Calling {self.get_poffin_permutations_2.__name__}, there are {'0' if poffins is None  else len(poffins)} poffins")
         return self._get_poffin_permutations_n(2, poffins)
 
     def get_poffin_permutations_3(self, poffins: list[Poffin] = None) -> tuple[Poffin, Poffin, Poffin]:  # noqa ES501
         """Every combination of 3 poffins"""
-        print(f"Calling {self.get_poffin_permutations_3.__name__}, there are {'0' if poffins is None  else len(poffins)} poffins")
         return self._get_poffin_permutations_n(3, poffins)
 
     def get_poffin_permutations_4(self, poffins: list[Poffin] = None) -> tuple[Poffin, Poffin, Poffin, Poffin]:  # noqa ES501
         """Every combination of 4 poffins"""
-        print(f"Calling {self.get_poffin_permutations_4.__name__}, there are {'0' if poffins is None  else len(poffins)} poffins")
         return self._get_poffin_permutations_n(4, poffins)
 
     def get_poffin_permutations_5(self, poffins: list[Poffin] = None) -> tuple[Poffin, Poffin, Poffin, Poffin, Poffin]:  # noqa ES501
         """Every combination of 5 poffins"""
-        print(f"Calling {self.get_poffin_permutations_5.__name__}, there are {'0' if poffins is None  else len(poffins)} poffins")
         return self._get_poffin_permutations_n(5, poffins)
 
     def get_poffin_permutations_10(self, poffins: list[Poffin]) -> tuple[Poffin, Poffin, Poffin, Poffin, Poffin]:  # noqa ES501
         """Every combination of 10 poffins"""
-        print(f"Calling {self.get_poffin_permutations_10.__name__}, there are {'0' if poffins is None  else len(poffins)} poffins")
         return self._get_poffin_permutations_n(10, poffins=None)
 
     def generate_custom_poffin_list_from_recipes(self, recipes) -> list[Poffin]:  # noqa ES501
